ada69348eb1975f2d25832ae37d8de15392d8360
Admin <bedasergey1991@gmail.com>
2021-02-24 01:26:03 +0300

    настройка подключения к БД

--- app/src/main/java/ru/Application.java
+++ app/src/main/java/ru/Application.java
@@ -2,11 +2,9 @@ package ru;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Generated
 @OpenAPIDefinition(info = @Info(title = "my service"))
 @SpringBootApplication
 public class Application {
